d527b27f8a84b7832d9bc3b876ebdab431840a88
Anita <ashrest58@gmail.com>
2026-09-10 22:33:55 -0400

    Update 06_SVG_detection.R

--- R_code/06_SVG_detection.R
+++ R_code/06_SVG_detection.R
@@ -19,17 +19,33 @@ DefaultAssay(obj) <- "Spatial.008um"
 obj <- FindSpatiallyVariableFeatures(obj,
   assay = "Spatial.008um",
   features = VariableFeatures(obj),
-  selection.method = "moransi")
+  selection.method = "moransi",
+  nfeatures = length(VariableFeatures(obj)))
 
-top_svg <- SpatiallyVariableFeatures(obj, method = "moransi")
-svg_info <- SVFInfo(obj, method = "moransi", status = TRUE)
-write.csv(svg_info, file.path(results_dir, "spatially_variable_genes.csv"), row.names = TRUE)
+svg_info <- SVFInfo(obj, method = "moransi")
+svg_info$MoransI_p.adj <- p.adjust(svg_info$MoransI_p.value, method = "BH")
 
-head(top_svg, 20)
+sum(!is.na(svg_info$MoransI_p.value))
+#20
+#FindSpatiallyVariableFeatures silently failed to compute Moran's I for all but 20 of the 
+#3000 requested genes - a reproducible behavior matching documented Seurat bug reportson 
+#high-resolution spatial data (issue#9087 and discussion#9608 - and we report results for 
+#the 20 genes it actually computed
+# Add gene names as a column, sort by adjusted P-value (most significant SVGs first)
+svg_info$gene <- rownames(svg_info)
+svg_info <- svg_info[order(svg_info$MoransI_p.adj), 
+                      c("gene", "MoransI_observed", "MoransI_p.value", "MoransI_p.adj")]
 
-png(file.path(results_dir, "top6_SVGs.png"), width = 2400, height = 1600, res = 150)
-print(SpatialFeaturePlot(obj, features = head(top_svg, 10), pt.size.factor = 8, ncol = 3))
-dev.off()
+write.csv(svg_info,
+          "/ddnlus/r2762/spatial/arabidopsis_visium/results/SVG_moransi_results.csv",
+          row.names = FALSE)
+
+#top1 is EXT3 which is a cell wall glycoprotein and has a role in root hair elongation and 
+# check EXT3 specifically
+svg_info["EXT3", ]
+#gene MoransI_observed MoransI_p.value MoransI_p.adj
+ EXT3        0.7907884               0             0
+#BH p-value adjustment was applied and confirmed that adjusted p-value is significant
 
 ## ---- 2. Overlap between top SVGs and cluster markers ---------------
 markers <- read.csv(file.path(results_dir, "cluster_markers.csv"), row.names = 1)
